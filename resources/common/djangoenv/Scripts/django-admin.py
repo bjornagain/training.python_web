b460dc392132f97918cc14db3abf154f3eb49e09
Steve Werner <steven.w.werner@gmail.com>
2014-02-18 16:15:11 -0800

    django tut

---
+++ resources/common/djangoenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Steve\Documents\GitHub\training.python_web\resources\common\djangoenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
